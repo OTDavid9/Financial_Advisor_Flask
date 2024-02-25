039389648af16ac2121d1401248fc8851d3fe201
OTDavid9 <123405351+OTDavid9@users.noreply.github.com>
2024-02-25 04:07:56 -0800

    Update fast_api.py

--- fast_api.py
+++ fast_api.py
@@ -49,8 +49,5 @@ async def ask_question(request: Request, query_request: QueryRequest):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
-
-# if __name__ == '__main__':
-#     uvicorn.run(app)
